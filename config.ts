02b735577463ca664de1b63da7f2de2652c1059c
David Salas <dasa.1998@gmail.com>
2026-03-25 22:34:58 -0500

    Refactorizacion de variables de entorno en firebase

--- config.ts
+++ config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // ⚠️ IMPORTANTE: Reemplaza estos valores con los que copiaste de tu consola de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBNKB9N6YIDYrSTOc2MhoObDCV4Zh1s4DA",
-  authDomain: "storenetos-backend.firebaseapp.com",
-  projectId: "storenetos-backend",
-  storageBucket: "storenetos-backend.firebasestorage.app",
-  messagingSenderId: "869101528217",
-  appId: "1:869101528217:web:c80aeb41d82fe63ceb4408"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Inicializar Firebase
